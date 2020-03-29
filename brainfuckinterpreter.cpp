94c3538466124197e7b937e05a2a6f5e8d1c8260
maddyb99 <43785086+maddyb99@users.noreply.github.com>
2020-03-29 18:26:58 +0530

    i/o interpreter for brainfuck

---
+++ brainfuckinterpreter.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    unsigned char cells[30000];
+    char ch;
+    for (int i = 0; i < 30000; i++)
+        cells[i] = 0;
+    int index = 0;
+    ifstream file;
+    file.open(argv[1]);
+    while (!file.eof())
+    {
+        file >> ch;
+        if (file.eof())
+            break;
+        // cout << ch;
+        switch (ch)
+        {
+        case '+':
+            cells[index]++;
+            // cout << cells[index];
+            break;
+        case '-':
+            cells[index]--;
+            // cout << int(cells[index]);
+            break;
+        case '.':
+            // cout << cells[index];
+            putchar(cells[index]);
+            // cout << endl;
+            break;
+        case ',':
+            cells[index] = getchar();
+            break;
+        case '>':
+            index++;
+            if (index == 30000)
+                index = 0;
+            break;
+        case '<':
+            index--;
+            if (index == -1)
+                index = 30000 - 1;
+            break;
+        default:
+            continue;
+        }
+    }
+    return 0;
+}
